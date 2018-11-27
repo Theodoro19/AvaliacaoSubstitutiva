4088e35a351af24392e8a5c1c40d99280c6e597f
Raphael Theodoro <raphaeltheodoro@hotmail.com>
2018-11-27 21:00:35 -0200

    Create

--- db/Create Table Address.sql
+++ db/Create Table Address.sql
@@ -1,11 +1,24 @@
-CREATE TABLE user
+CREATE TABLE address
 (
-cd_id character varying(45) NOT NULL,
-
-CONSTRAINT add_pkey PRIMARY KEY (cd_id)
+cd_id bigserial NOT NULL,
+user_id bigint,
+street_name character varying(255),
+number character varying(6),
+complement character varying(100),
+neighborhood character varying(100),
+city_id bigint,
+postal_code character varying(10),
+address_name character varying(45),
+CONSTRAINT address_pkey PRIMARY KEY (cd_id),
+  CONSTRAINT address_user_id_fkey FOREIGN KEY (user_id)
+      REFERENCES user (user_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+	    CONSTRAINT address_cities_id_fkey FOREIGN KEY (city_id)
+      REFERENCES cities (cd_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE user
+ALTER TABLE address
   OWNER TO postgres;
--- db/Create Table Cities.sql
+++ db/Create Table Cities.sql
@@ -1,21 +1,15 @@
-CREATE TABLE user
+CREATE TABLE cities
 (
-user_id character varying(45) NOT NULL,
-phone_number character varying(20),
-email character varying(255),
-first_name character varying(100),
-last_name character varying(100),
-date_of_birth character varying(45),
-national_id character varying(15),
-phone_validated boolean NOT NULL DEFAULT true,
-email_confirmed boolean NOT NULL DEFAULT true,
-create_time timestamp with time zone,
-password_enc character varying(100),
-password_salt character varying(50),
-CONSTRAINT user_pkey PRIMARY KEY (user_id)
+cd_id bigserial NOT NULL,
+state_id bigint,
+city_name character varying(255),
+CONSTRAINT cities_pkey PRIMARY KEY (cd_id),
+	    CONSTRAINT cities_states_id_fkey FOREIGN KEY (state_id)
+      REFERENCES states (cd_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE user
+ALTER TABLE cities
   OWNER TO postgres;
