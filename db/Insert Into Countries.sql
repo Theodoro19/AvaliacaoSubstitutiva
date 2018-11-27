95360258d96ed0e22769c06a39cc53e6efafe95f
Theodoro19 <raphaeltheodoro@hotmail.com>
2018-11-27 21:20:42 -0200

    Update Insert Into Countries.sql

---
+++ db/Insert Into Countries.sql
@@ -0,0 +1,5 @@
+insert into countries (country_code,country_name_br,country_name_en) values ('BR','Brasil','Brazil');
+insert into countries (country_code,country_name_br,country_name_en) values ('IT','Itália','Italy');
+insert into countries (country_code,country_name_br,country_name_en) values ('EN','Inglaterra','England');
+insert into countries (country_code,country_name_br,country_name_en) values ('ES','Espanha','Spain');
+insert into countries (country_code,country_name_br,country_name_en) values ('AL','Alemanha','Germany');
