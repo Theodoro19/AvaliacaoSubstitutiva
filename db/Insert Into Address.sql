8ae928ea2c8aaf12a43cf6971d810b40a1e360b6
Theodoro19 <raphaeltheodoro@hotmail.com>
2018-11-27 21:21:28 -0200

    Update Insert Into Address.sql

---
+++ db/Insert Into Address.sql
@@ -0,0 +1,2 @@
+insert into address (user_id, street_name, number, complement, neighborhood, city_id, postal_code, address_name)
+values (1, 'Rua', '586', 'Casa', 'Hospital Samaritano', 1, '01233-000', 'Tupi,')
