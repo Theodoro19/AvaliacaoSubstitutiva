20b5c822a2b646933ce1fbc81cdb231465b0e628
Raphael Theodoro <raphaeltheodoro@hotmail.com>
2018-11-27 18:56:31 -0200

    Address e Cities Model

--- avaliacao.substitutiva/src/main/java/br/com/theodoro/avaliacao/address/model/AddressRepository.java
+++ avaliacao.substitutiva/src/main/java/br/com/theodoro/avaliacao/address/model/AddressRepository.java
@@ -1,5 +1,8 @@
 package br.com.theodoro.avaliacao.address.model;
 
-public interface AddressRepository {
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AddressRepository extends CrudRepository<Address, Long>, JpaSpecificationExecutor<Address> {
 
 }
