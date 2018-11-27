00a9b20d5a8db15dc1f9a6351456db6af8b1ddb4
Theodoro19 <raphaeltheodoro@hotmail.com>
2018-11-27 21:21:54 -0200

    Update Insert Into User.sql

---
+++ db/Insert Into User.sql
@@ -0,0 +1,15 @@
+insert into user phone_number, email, first_name, last_name, date_of_birth, national_id, phone_validated,
+email_confirmed, create_time, password_enc, password_salt) values ('1','11971145252','raphaeltheodoro@hotmail.com',
+'Raphael','Theodoro','19/11/1991','brasileiro',true, true, now(), '123456','123456')
+insert into user phone_number, email, first_name, last_name, date_of_birth, national_id, phone_validated,
+email_confirmed, create_time, password_enc, password_salt) values ('2','11971145252','raphaeltheodoro@hotmail.com',
+'Raphael','Pereira','19/11/1991','brasileiro',true, true, now(), '123456','123456')
+insert into user phone_number, email, first_name, last_name, date_of_birth, national_id, phone_validated,
+email_confirmed, create_time, password_enc, password_salt) values ('3','11971145252','raphaeltheodoro@hotmail.com',
+'Raphael','Camargo','19/11/1991','brasileiro',true, true, now(), '123456','123456')
+insert into user phone_number, email, first_name, last_name, date_of_birth, national_id, phone_validated,
+email_confirmed, create_time, password_enc, password_salt) values ('4','11971145252','raphaeltheodoro@hotmail.com',
+'Raphael','Nakaguchi','19/11/1991','brasileiro',true, true, now(), '123456','123456')
+insert into user phone_number, email, first_name, last_name, date_of_birth, national_id, phone_validated,
+email_confirmed, create_time, password_enc, password_salt) values ('5','11971145252','raphaeltheodoro@hotmail.com',
+'Raphael','Paiani','19/11/1991','brasileiro',true, true, now(), '123456','123456')
